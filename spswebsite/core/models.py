75594fcdb15baa742b7d58dcb73152ea4ff0063f
Piotr Zacha <pzacha11@gmail.com>
2020-05-08 14:21:45 +0200

    created event model

--- spswebsite/core/models.py
+++ spswebsite/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    desc = models.TextField("Event description", blank=True, max_length=1500)
